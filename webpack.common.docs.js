bad1c09544c4ff23273fdb4b99ae4505bbfbee78
Skarley Guerrero <skarleyguerrero@gmail.com>
2018-11-27 19:15:42 +0000

    added docs folder and webpack script

---
+++ webpack.common.docs.js
@@ -0,0 +1,72 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'docs'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        },
+        { test: /\.md$/, use: [
+              {
+                  loader: "html-loader"
+              },
+              {
+                  loader: "markdown-loader",
+                  options: {
+                      /* your options here */
+                  }
+              }
+          ]
+        },
+        {
+          test: /\.(scss|css)$/, use: [{
+              loader: "style-loader" // creates style nodes from JS strings
+          }, {
+              loader: "css-loader" // translates CSS into CommonJS
+          }, {
+              loader: "sass-loader" // compiles Sass to CSS
+          }]
+        }, //css only files
+        {
+          test: /\.(gif|png|jpe?g|svg)$/i,
+          use: [{
+            loader: 'file-loader?name=[name].[ext]',
+            options: {
+              name: 'images/[name].[ext]',
+              emitFile: true
+            }
+          }],
+        }, //for images
+        {
+          test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+          use: ['file-loader','url-loader?limit=100000']
+        } //for fonts
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProvidePlugin({
+      // In case you imported plugins individually, you must also require them here:
+      Util: "exports-loader?Util!bootstrap/js/dist/util",
+      Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+    }),
+    new HtmlWebpackPlugin({
+        favicon: 'AMS.ico',
+        template: 'template.html'
+    })
+  ]
+};
